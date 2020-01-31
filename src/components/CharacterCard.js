2612a5be5620273c3d638d2b8d2413f85086ab65
Cristina Edens <edens.cristina@gmail.com>
2020-01-31 12:48:26 -0500

    Got it working, adding styling now

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+
+  return (
+    <div className="card-container">
+      <div className="card">
+        <img src={props.character.image} />
+        <div className="info-container">
+          <h2>Name: {props.character.name}</h2>
+          <h3>Gender: {props.character.gender}</h3>
+          <p>Species: {props.character.species}</p>
+          <p>Origin: {props.character.origin.name}</p>
+        </div>
+        <div>Status: {props.character.status}</div>
+      </div>
+    </div>
+  )
 }
